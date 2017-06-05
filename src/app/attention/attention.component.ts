36b1b9abb94669df42a85d4b5e01fb4661a325c8
pascal0828 <pascal0828@gmail.com>
2017-06-04 23:34:42 -0400

    Added Simulation Mode
    Added simulation mode for debugging without a headset

--- src/app/attention/attention.component.ts
+++ src/app/attention/attention.component.ts
@@ -1,21 +1,54 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs/Rx';
 import * as io from 'socket.io-client';
 
 @Component({
   selector: 'attention',
   template: `
-  <div class="ball-container" [style.width.vw]="maxWidth | async">
-  <div class="ball"  [style.width.px]="circleSize |async" [style.height.px]="circleSize | async" [ngStyle]="attentionTransform | async">
-    <span [style.fontSize.px]="valueInsideCircle | async" class="valueInsideCircle">{{ valueInsideCircle | async }} </span>
-  </div>
+  <div class="attention-container">
+      <div class="ball-container">
+        <div class="ball"  [style.width.px]="circleSize" [style.height.px]="circleSize" [ngStyle]="attentionTransform">
+          <span [style.fontSize.px]="valueInsideCircle" class="valueInsideCircle">{{valueInsideCircle}} </span>
+        </div>
+      </div>
+        <div class="buttons-container">
+        <button (click)="simulateBrainwaves()" [hidden]="brainTimerSubscription">Simulate Brainwaves</button>
+        <button (click)="cancelSimulation()" [hidden]="!brainTimerSubscription">Cancel Simulation</button>
+      </div>
   </div>
   `,
-  
+
   styles: [`
+  .attention-container {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+    overflow:hidden;
+  }
 
+  button {
+    height:40px;
+    color: #000;
+    background-color: #EEE;
+    padding: 0 6px 0 6px;
+    margin: 6px 8px 6px 8px;
+    min-width: 88px;
+    border-radius: 3px;
+    font-size: 14px;
+    text-align: center;
+    text-transform: uppercase;
+    text-decoration:none;
+    border: none;
+    outline: none;
+  }
+  .buttons-container{
+    text-align:center;
+    padding-top: 200px;
+  }
   .ball-container {
-
+    display:flex;
+    flex:1;
+    width:100vw;
   }
 
   .ball {
@@ -27,8 +60,6 @@ import * as io from 'socket.io-client';
     display: flex;
     align-items: center;
     justify-content: center;
-
-
   }
   .valueInsideCircle {
     color: white;
@@ -43,19 +74,46 @@ import * as io from 'socket.io-client';
   `]
 })
 export class AttentionComponent {
+  attention: BehaviorSubject<any> = new BehaviorSubject<any>(0);
   offset = 60;
   socket = io('http://localhost:4501');
   brainwaves = Observable.fromEvent<any>(this.socket, 'metric:eeg');
+  attentionTransform: any;
   image = 'https://pbs.twimg.com/media/CiEn5P_WEAMn5wT.jpg';
+  brainTimerSubscription: Subscription;
 
-  valueInsideCircle = this.brainwaves.map(eeg => eeg.eSense.attention);
-  circleSize = this.brainwaves.map(eeg => eeg.eSense.attention * 2);
+  valueInsideCircle: number;
+  circleSize: number;
+  simulatedValue: number = 100;
 
-  attentionTransform = this.brainwaves
-    .map(eeg => ({
-        transform: `translateX(${ eeg.eSense.attention }vw)`
-    }));
-  
+  constructor() {
+    this.brainwaves.subscribe(eeg => {
+      this.attention.next(eeg.eSense.attention);
+    });
+
+    this.attention.subscribe(attention => {
+      this.valueInsideCircle = attention;
+      this.circleSize = attention * 2;
+      this.attentionTransform = { transform: `translateX(${attention}vw)`, 'margin-left' : `-${this.circleSize / 2}px` };
+    });
+
+    this.attention.next(50);
+  }
+
+  simulateBrainwaves() {
+    this.brainTimerSubscription = Observable.timer(0, 500).subscribe(value => {
+      this.simulatedValue = Math.max(Math.min(Math.floor(this.simulatedValue + (Math.random() * 15) - (Math.random() * 25)), 100), 10);
+      this.attention.next(this.simulatedValue);
+    });
+  }
+
+  cancelSimulation() {
+    this.brainTimerSubscription.unsubscribe();
+    this.brainTimerSubscription = null;
+    this.attention.next(50);
+    this.simulatedValue = 50;
+  }
+
 }
 
 
