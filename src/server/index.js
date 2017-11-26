098e5f9932ef81c876af9fbcd379a04274f2eab4
Alex Castillo <alex@castillo.io>
2017-11-26 11:50:01 -0800

    updated rxjs to v5.5.2

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,16 @@
 
 const thinkgear = require('node-thinkgear-sockets');
-const { Observable } = require('rxjs/Rx');
+const { fromEvent } = require('rxjs/observable/fromEvent');
 const io = require('socket.io')(4501);
 
 const client = thinkgear.createClient();
 
 client.connect();
 
-const brainwaves = Observable.fromEvent(client, 'data')
+fromEvent(client, 'data')
     .subscribe(data => sendToBrowser(data, 'eeg'));
 
-const blinks = Observable.fromEvent(client, 'blink_data')
+fromEvent(client, 'blink_data')
     .subscribe(data => sendToBrowser(data, 'blinks'));
 
 function sendToBrowser (data, metric) {
