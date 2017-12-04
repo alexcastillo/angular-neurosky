9792a5c8098c7f27e964ba951a14c9b7bd003169
Alex Castillo <alex@castillo.io>
2017-12-04 16:03:10 +0100

    playback fixes

--- src/app/mind-video-player/mind-video-player.component.ts
+++ src/app/mind-video-player/mind-video-player.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import { interval } from 'rxjs/observable/interval';
 import { from } from 'rxjs/observable/from';
-import { map, tap, zip, mergeMap } from 'rxjs/operators';
+import { map, tap, zip, switchMap } from 'rxjs/operators';
 import { createMock } from '../shared/mock';
 import * as io from 'socket.io-client';
 import linspace from 'linspace';
@@ -30,12 +30,12 @@ export class MindVideoPlayerComponent {
     map((eeg: any) => Math.abs(eeg.eSense[this.metricName] - this.video.offset))
   );
   currentTime$ = this.metric$.pipe(
-    mergeMap(metric => {
+    switchMap(metric => {
       const range = from(linspace(this.prevMetric, metric, this.video.fps));
       this.prevMetric = metric;
       return range;
     }),
-    zip(interval(1000 / this.video.fps), x => x),
+    zip(interval(this.video.fps), x => x),
     map((metric: any) => Math.abs((this.video.length * metric) / 100))
   );
 }
