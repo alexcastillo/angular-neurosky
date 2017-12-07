090d211fcf297a832d58b2a30ceff9f7782acae5
Alex Castillo <alex@castillo.io>
2017-12-07 10:57:35 +0100

    change default video in routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ const appRoutes: Routes = [
     component: MindVideoPlayerComponent,
   },
   { path: '',
-    redirectTo: '/attention/coffee',
+    redirectTo: '/attention/beer',
     pathMatch: 'full'
   }
 ];
